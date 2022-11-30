3918f82acd8710f2d06c3621a36f5d379c83d404
anthony <quereantho@gmail.com>
2022-11-30 18:06:09 +0100

    :sparkles: multi-file support

--- icmp/listener/listener.go
+++ icmp/listener/listener.go
@@ -3,16 +3,12 @@ package main
 import (
 	"encoding/binary"
 	"errors"
-	"fmt"
+	"github.com/Anthony-Jhoiro/cyber-extractor/commons"
 	icmpcommons "github.com/Anthony-Jhoiro/cyber-extractor/icmp"
 	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv4"
 	"log"
-	"os"
-	"path"
 	"reflect"
-	"sort"
-	"strconv"
 )
 
 const (
@@ -53,48 +49,6 @@ func readTram(data []byte) (uint32, uint32, []byte) {
 	return binary.LittleEndian.Uint32(data[0:4]), binary.LittleEndian.Uint32(data[4:8]), data[8:]
 }
 
-func buildFile(id uint32) (string, error) {
-	tmpFileDir := fmt.Sprintf("results/%d", id)
-	fileInfos, err := os.ReadDir(tmpFileDir)
-	if err != nil {
-		return "", err
-	}
-
-	file, err := os.Create(fmt.Sprintf("results/%d.raw", id))
-
-	defer file.Close()
-
-	sort.Slice(fileInfos, func(i, j int) bool {
-		f1, err := strconv.Atoi(fileInfos[i].Name())
-		if err != nil {
-			return false
-		}
-
-		f2, err := strconv.Atoi(fileInfos[j].Name())
-		if err != nil {
-			return false
-		}
-		return f1 < f2
-	})
-
-	for _, fileInfo := range fileInfos {
-		if !fileInfo.Type().IsDir() {
-			content, err := os.ReadFile(path.Join(tmpFileDir, fileInfo.Name()))
-			if err != nil {
-				return "", err
-			}
-			_, err = file.Write(content)
-			if err != nil {
-				return "", err
-			}
-		}
-	}
-
-	os.RemoveAll(tmpFileDir)
-
-	return file.Name(), nil
-}
-
 func readRequests(conn *icmp.PacketConn) chan string {
 	ch := make(chan string)
 
@@ -111,23 +65,14 @@ func readRequests(conn *icmp.PacketConn) chan string {
 			if reflect.DeepEqual(payload, icmpcommons.StopSequence) {
 				log.Printf("Recieved Stop file\n")
 
-				fileName, err := buildFile(id)
+				fileName, err := commons.BuildFile(id)
 				if err != nil {
 					log.Printf("[ERROR] %v\n", err)
 				}
 
 				ch <- fileName
 			} else {
-				log.Printf("Recieve [%d] - No %d - %d bytes\n", id, seq, len(payload))
-
-				os.Mkdir(fmt.Sprintf("results/%d", id), 0777)
-				file, err := os.Create(fmt.Sprintf("results/%d/%d", id, seq))
-				if err != nil {
-					panic(err)
-				}
-				file.Write(payload)
-
-				file.Close()
+				commons.WriteByteFile(id, seq, payload)
 			}
 		}
 	}()
